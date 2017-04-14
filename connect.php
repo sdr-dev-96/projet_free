da4087013ba78736f4763550713d8cd1d0609a0b
Stefane Rodrigues <Stefane Rodrigues>
2017-04-14 07:50:27 +0200

    second commit

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php 
+require_once('inc/header.html');
+require_once('inc/footer.html');
+
+ ?>
+ <div class="container connect">
+ 	<div class="row">
+ 		<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+ 			<img src="img/logo_free.png" alt="Free" width="110">
+ 		</div>
+ 	</div>
+ 	<div class="row">
+ 		<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+ 			<h3>Connectez-vous</h3>
+ 		</div>
+ 	</div>
+ 	<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+ 		<input id="username" type="text" class="form-control" placeholder="email@free.fr"> 		
+ 		<input id="password" type="text" class="form-control" placeholder="Mot de passe">
+ 	</div>
+ 	
+ </div>
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+require_once('inc/header.html');
+require_once('inc/footer.html');
+?>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">
+					<span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+				</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<div class="nav navbar-nav navbar">
+					<a class="navbar-brand" href="#">
+						<img alt="Free" src="img/logo_free.png" width="65">
+					</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a href="#">
+							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+						</a>
+					</li>
+					<li>
+						<a href="connect.php">
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+							<span class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Paramètres de l'application</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Noter Free Compagnon</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">A propos de Free Compagnon</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+				<div class="col-sm-3 icon_service col-xs-4">
+					
+				</div>
+				<div class="col-sm-9 text-center libelle_service col-xs-8">
+					<h3>TV</h3>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+				<div class="col-sm-3 icon_service col-xs-4">
+					
+				</div>
+				<div class="col-sm-9 text-center libelle_service col-xs-8">
+					<h3>Téléphonie</h3>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+				<div class="col-sm-3 icon_service col-xs-4">
+					
+				</div>
+				<div class="col-sm-9 text-center libelle_service col-xs-8">
+					<h3>Internet</h3>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+				<div class="col-sm-3 icon_service col-xs-4">
+					
+				</div>
+				<div class="col-sm-9 text-center libelle_service col-xs-8">
+					<h3>Réglages</h3>
+				</div>
+			</div>
+		</div>
+	</div>
