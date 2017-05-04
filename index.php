7c8bc850058c65ac62a2e5fc98e5318bad983e40
Stefane Rodrigues <Stefane Rodrigues>
2017-05-04 11:14:22 +0200

    ajout chaines + modifs fichiers inc

--- index.php
+++ index.php
@@ -1,53 +1,38 @@
 <?php
-require_once('inc/header.html');
+require_once('inc/header_home.html');
 require_once('inc/footer.html');
 ?>
-<body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header left-navbar free_home">
-				<a class="navbar-brand" href="index.php">
-				</a>
-			</div>
-			<div class="navbar-header right-navbar">
-				<a href="#" class="navbar-brand">					
-					<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-				</a>
-				<a href="connect.php" class="navbar-brand">
-					<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-				</a>
-				<a href="#" class="navbar-brand">
-					<span class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
-				</a>
-			</div>
-		</div>
-	</nav>
 	<div class="container">
 	<?php 
 		$services = array(
 			1 => array(
 				'libelle' => 'TV',
-				'page' => 'tv.php',),
+				'page' => 'tv.php',
+				'picto' => '<img src="img/picto_tv.svg" width="80">'),
 			2 => array(
 				'libelle' => 'Téléphonie',
-				'page' => 'telephonie.php'),
+				'page' => 'telephonie.php',
+				'picto' => '<span class="glyphicon glyphicon-earphone"></span>'),
 			3 => array(
 				'libelle' => 'Internet',
-				'page' => 'internet.php'),
+				'page' => 'internet.php',
+				'picto' => '<span class="glyphicon glyphicon-globe"></span>'),
 			4 => array(
 				'libelle' => 'Réglages',
-				'page' => 'reglages.php'));
+				'page' => 'reglages.php',
+				'picto' => '<span class="glyphicon glyphicon-cog"></span>'));
 		foreach($services as $s){
 	 ?>
 		<div class="row">
 			<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
 				<div class="col-sm-3 icon_service col-xs-4" onclick="location.href='<?php echo $s['page'] ;?>'">
-					<img src="img/picto_tv.svg" width="80">
+					<?php echo $s['picto']; ?>
 				</div>
-				<div class="col-sm-9 text-center libelle_service col-xs-8">
-					<h3><?php echo $s['libelle']; ?></h3>
+				<div class="col-sm-9 text-center libelle_service col-xs-8" onclick="location.href='<?php echo $s['page'] ;?>'">
+					<h5><?php echo $s['libelle']; ?></h5>
 				</div>
 			</div>
 		</div>
 		<?php } ?>
 	</div>
+	
--- tv.php
+++ tv.php
@@ -1,32 +1,7 @@
 <?php 
-require_once('inc/header.html');
+require_once('inc/header_service.html');
 require_once('inc/footer.html');
  ?>
- <nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header left-navbar">
-				<a class="navbar-brand" href="#">
-					<span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
-				</a>
-			</div>
-			<div class="navbar-header left-navbar free_home">
-				<a class="navbar-brand" href="index.php">
-					
-				</a>
-			</div>
-			<div class="navbar-header right-navbar">
-				<a href="#" class="navbar-brand">					
-					<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-				</a>
-				<a href="connect.php" class="navbar-brand">
-					<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-				</a>
-				<a href="#" class="navbar-brand">
-					<span class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
-				</a>
-			</div>
-		</div>
-	</nav>
 	<div class="container" id="programmes_tv">
 	<?php 
 		$programmes = array(
@@ -34,28 +9,73 @@ require_once('inc/footer.html');
 				'nom' => 'Esprits Criminels',
 				'heure_debut' => '20h50',
 				'heure_fin' => '23h00',
-				'width' => 'width: 60%;'),
+				'width' => 'width: 60%;',
+				'chaine' => 'tf1'),
 			2 => array(
 				'nom' => 'Plus Belle la vie',
 				'heure_debut' => '21h00',
 				'heure_fin' => '22h45',
-				'width' => 'width: 40%;'),
+				'width' => 'width: 40%;',
+				'chaine' => 'france2'),
 			3 => array(
 				'nom' => 'Slam',
 				'heure_debut' => '20h30',
 				'heure_fin' => '21h00',
-				'width' => 'width: 75%;')
+				'width' => 'width: 75%;',
+				'chaine' => 'france3'),
+			4 => array(
+				'nom' => 'Le Grand Journal',
+				'heure_debut' => '19h05',
+				'heure_fin' => '20h45',
+				'width' => 'width: 80%',
+				'chaine' => 'canalplus'),
+			5 => array(
+				'nom' => 'C à vous',
+				'heure_debut' => '20h50',
+				'heure_fin' => '23h00',
+				'width' => 'width: 60%;',
+				'chaine' => 'france5'),
+			6 => array(
+				'nom' => 'L\'amour est dans le pré',
+				'heure_debut' => '20h50',
+				'heure_fin' => '23h00',
+				'width' => 'width: 60%;',
+				'chaine' => 'm6'),
+			7 => array(
+				'nom' => 'Histoire de l\'Europe',
+				'heure_debut' => '20h50',
+				'heure_fin' => '23h00',
+				'width' => 'width: 60%;',
+				'chaine' => 'arte'),
+			8 => array(
+				'nom' => 'Touche pas à mon Poste',
+				'heure_debut' => '19h05',
+				'heure_fin' => '21h05',
+				'width' => 'width: 55%;',
+				'chaine' => 'c8'),
+			9 => array(
+				'nom' => 'Les Marseillais : South America',
+				'heure_debut' => '19h50',
+				'heure_fin' => '20h40',
+				'width' => 'width: 63%;',
+				'chaine' => 'w9'),
+			10 => array(
+				'nom' => 'Le Quotidien',
+				'heure_debut' => '19h30',
+				'heure_fin' => '20h45',
+				'width' => 'width: 58%;',
+				'chaine' => 'tmc')
 			);
 		foreach ($programmes as $p) {
 			?>
 			<div class="row">
 				<div class="col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-					<div class="col-sm-3 chaine col-xs-4">
+					<div class="col-sm-3 chaine col-xs-4" id="<?php echo $p['chaine']; ?>">
 
 					</div>
 					<div class="col-sm-9 text-center programme col-xs-8">
 						<h4><?php echo $p['nom']; ?></h4>
-						<h5><?php echo $p['heure_debut'] . ' - ' . $p['heure_fin'];  ?></h5>
+						<h6><?php echo $p['heure_debut'] . ' - ' . $p['heure_fin'];  ?></h6>
 						<div class="progress">
 							<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="<?php echo $p['width']; ?>">
 							</div>
@@ -75,35 +95,42 @@ require_once('inc/footer.html');
 		Sorya BORGHI
 		</div>
 		<div class="row">
-		Reprendre la lecture
-		<br><br>
-		Mes enregistrements
+		TV
+			<ul>
+				<li>
+					<a href="#">
+						<span class="glyphicon glyphicon-play" aria-hidden="true"></span>
+						Live
+					</a>					
+				</li>
+				<li>
+					<a href="#">
+						<span class="glyphicon glyphicon-list" aria-hidden="true"></span>
+						Programmes
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<span class="glyphicon glyphicon-backward" aria-hidden="true"></span>
+						Replay
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<span class="glyphicon glyphicon-film" aria-hidden="true"></span>
+						VOD
+					</a>
+				</li>
+			</ul>
 		</div>
 		<div class="row">
-		Catégories
-		<br><br>
-		Regarder la télé
-		<br>
-		Chaînes
-		<br>
-		Films
-		<br>
-		Séries
-		<br>
-		Sport
-		<br>
-		Informations
-		<br>
-		Documentaires
-		<br>
-		Culture
-		<br>
-		Divertissement
-		<br>
-		Enfants
-		</div>
-		<div class="row">
-		Réglages
+		Téléphonie
+			<ul>
+				<li>Live</li>
+				<li>Programmes</li>
+				<li>Replay</li>
+				<li>VOD</li>
+			</ul>
 		</div>
 
 	</div>
